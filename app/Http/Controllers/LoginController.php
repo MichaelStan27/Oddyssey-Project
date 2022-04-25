fd8de6e3e1f093bba3940370b23104dc036b0a3a
Daniel Wijaya <dwjy27@gmail.com>
2022-04-25 19:53:38 +0700

    Validate login field and show appropriate error message

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,4 +9,12 @@ class LoginController extends Controller {
     public function index() {
         return view('login');
     }
+
+    public function store(Request $request) {
+        // This will throw exception if validation fails
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8']
+        ]);
+    }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,15 +5,23 @@
 @section('content')
     <div class="container bg-white mx-auto rounded-lg shadow-md p-6 w-1/4">
         <form action="/login" method="POST">
+            @csrf
             <div class="mb-4">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email"
-                    class="rounded-md w-full p-2 bg-gray-100 border-gray-200 border-2 outline-purple-300">
+                    class="rounded-md w-full p-2 bg-gray-100 border-2 outline-purple-300 @error('email') border-red-500 @enderror"
+                    value="{{ old('email') }}">
+                @error('email')
+                    <p class="text-red-500 text-sm">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-4">
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password"
-                    class="rounded-md w-full p-2 bg-gray-100 border-gray-200 border-2 outline-purple-300">
+                    class="rounded-md w-full p-2 bg-gray-100 border-2 outline-purple-300 @error('password') border-red-500 @enderror">
+                @error('password')
+                    <p class="text-red-500 text-sm">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-4">
                 <input type="checkbox" name="remember" id="remember">

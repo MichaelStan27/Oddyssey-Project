a183848a07aac70eb8fa376decb955bd70f3d12f
Daniel Wijaya <dwjy27@gmail.com>
2022-04-25 19:53:24 +0700

    Add login post route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/', function () {
     return view('home');

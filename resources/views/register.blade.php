3e93eae2bce23c8b40d5be2c9cd50a6768b90bdd
Daniel Wijaya <dwjy27@gmail.com>
2022-04-25 19:47:04 +0700

    Remove duplicate border style class

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,7 +9,7 @@
             <div class="mb-4">
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name"
-                    class="rounded-md w-full p-2 bg-gray-100 border-gray-200 border-2 outline-purple-300 @error('name') border-red-500 @enderror"
+                    class="rounded-md w-full p-2 bg-gray-100 border-2 outline-purple-300 @error('name') border-red-500 @enderror"
                     value="{{ old('name') }}">
                 @error('name')
                     <p class="text-red-500 text-sm">{{ $message }}</p>
@@ -18,7 +18,7 @@
             <div class="mb-4">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email"
-                    class="rounded-md w-full p-2 bg-gray-100 border-gray-200 border-2 outline-purple-300 @error('email') border-red-500 @enderror"
+                    class="rounded-md w-full p-2 bg-gray-100 border-2 outline-purple-300 @error('email') border-red-500 @enderror"
                     value="{{ old('email') }}">
                 @error('email')
                     <p class="text-red-500 text-sm">{{ $message }}</p>
@@ -27,7 +27,7 @@
             <div class="mb-4">
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password"
-                    class="rounded-md w-full p-2 bg-gray-100 border-gray-200 border-2 outline-purple-300 @error('password') border-red-500 @enderror">
+                    class="rounded-md w-full p-2 bg-gray-100 border-2 outline-purple-300 @error('password') border-red-500 @enderror">
                 @error('password')
                     <p class="text-red-500 text-sm">{{ $message }}</p>
                 @enderror
@@ -35,7 +35,7 @@
             <div class="mb-4">
                 <label for="password_confirmation">Confirm Password</label>
                 <input type="password" name="password_confirmation" id="password_confirmation"
-                    class="rounded-md w-full p-2 bg-gray-100 border-gray-200 border-2 outline-purple-300 @error('password') border-red-500 @enderror">
+                    class="rounded-md w-full p-2 bg-gray-100 border-2 outline-purple-300 @error('password') border-red-500 @enderror">
             </div>
             <div class="flex justify-end gap-4 items-center">
                 <a href="{{ route('login') }}" class="underline">Already registered?</a>

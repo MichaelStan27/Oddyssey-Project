22a8dc0f82631a4577dcd4202b133cf64141423d
Michael Stanley <michstan96@gmail.com>
2022-04-26 18:31:03 +0700

    remove auth middleware in game controller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,10 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class GameController extends Controller {
-    public function __construct() {
-        $this->middleware('auth');
-    }
-
     public function index() {
         return view('game');
     }

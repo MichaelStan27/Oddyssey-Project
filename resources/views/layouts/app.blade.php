eeac4e42ab6c911af222fa4414adaecab4dc940c
Daniel Wijaya <dwjy27@gmail.com>
2022-04-26 23:24:41 +0700

    Add interactivity to dropdown menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <script src="{{ asset('/js/app.js') }}" defer></script>
     <title>Oddyssey | @yield('title')</title>
 </head>
 

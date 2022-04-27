9cfb37486e9dced5b2eedf258e3307230cc22d1e
Daniel Wijaya <dwjy27@gmail.com>
2022-04-27 15:21:38 +0700

    Connect game card to game details page

--- resources/views/components/game-card-square.blade.php
+++ resources/views/components/game-card-square.blade.php
@@ -7,12 +7,14 @@ $desc = $game['description'];
 $price = $game['price'] == 0 ? 'FREE' : 'IDR ' . $game['price'] * 1000;
 @endphp
 
-<div class="bg-white rounded-xl overflow-hidden relative">
-    <img src="{{ asset('/assets/games/' . $image . '/thumb.jpg') }}" class="mb-4">
-    <div class="w-full my-4 px-4">
-        <h1 class="font-medium text-lg mb-2">{{ $title }}</h1>
-        <p class="text-gray-500 mb-14">{{ Str::limit($desc, 100, $end = '...') }}</p>
-        <p class="text-right text-lg text-gray-700 font-semibold absolute right-4 bottom-2">
-            {{ $price }}</p>
+<a href="{{ route('game', $game) }}">
+    <div class="bg-white rounded-xl overflow-hidden relative h-full">
+        <img src="{{ asset('/assets/games/' . $image . '/thumb.jpg') }}" class="mb-4">
+        <div class="w-full my-4 px-4">
+            <h1 class="font-medium text-lg mb-2">{{ $title }}</h1>
+            <p class="text-gray-500 mb-14">{{ Str::limit($desc, 100, $end = '...') }}</p>
+            <p class="text-right text-lg text-gray-700 font-semibold absolute right-4 bottom-2">
+                {{ $price }}</p>
+        </div>
     </div>
-</div>
+</a>

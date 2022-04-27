b413028e4ec6132945652c8b3a2f096160e4d8c4
Daniel Wijaya <dwjy27@gmail.com>
2022-04-27 15:30:30 +0700

    Connect game card rect with game detail page

---
+++ resources/views/components/game-card-rect.blade.php
@@ -0,0 +1,21 @@
+@props(['game' => $game])
+
+@php
+$image = $game['image'];
+$title = $game['title'];
+$cat = $game->category->name;
+$price = $game['price'] == 0 ? 'FREE' : 'IDR ' . $game['price'] * 1000;
+@endphp
+
+<a href="{{ route('game', $game) }}" class="block">
+    <div class="bg-white rounded-lg shadow-lg overflow-hidden flex items-center justify-between h-ful">
+        <div class="flex max-w-fit">
+            <img src="{{ asset('/assets/games/' . $image . '/thumb.jpg') }}" class="h-24">
+            <div class="m-5 self-center">
+                <h1 class="font-medium text-xl">{{ $title }}</h1>
+                <p class="text-gray-500">{{ $cat }}</p>
+            </div>
+        </div>
+        <p class="text-right mr-4 font-semibold text-lg text-gray-600">{{ $price }}</p>
+    </div>
+</a>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,24 +17,8 @@
         <h1 class="font-bold text-2xl mb-4">Hot Games</h1>
         <div class="container flex flex-col gap-4">
             @for ($i = 0; $i < 8; $i++)
-                @php
-                    $image = $games[$i]['image'];
-                    $title = $games[$i]['title'];
-                    $cat = $games[$i]->category->name;
-                    $price = $games[$i]['price'] == 0 ? 'FREE' : 'IDR ' . $games[$i]['price'] * 1000;
-                @endphp
-                <div class="bg-white rounded-lg shadow-lg overflow-hidden flex items-center justify-between">
-                    <div class="flex w-[30%]">
-                        <img src="{{ asset('/assets/games/' . $image . '/thumb.jpg') }}" class="w-1/2">
-                        <div class="m-5 self-center">
-                            <h1 class="font-medium text-xl">{{ $title }}</h1>
-                            <p class="text-gray-500">{{ $cat }}</p>
-                        </div>
-                    </div>
-                    <p class="text-right mr-4 font-semibold text-lg text-gray-600">{{ $price }}</p>
-                </div>
+                <x-game-card-rect :game="$games[$i]"></x-game-card-rect>
             @endfor
         </div>
     </div>
-
 @endsection

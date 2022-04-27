a03ce9bc1e5c3560e7172ee6428c6ed8b557041e
Daniel Wijaya <dwjy27@gmail.com>
2022-04-27 23:20:05 +0700

    Make nav tab indicate active page

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,12 @@
 <nav class="bg-white flex justify-between px-36 mb-5 font-medium text-lg text-gray-600">
     <ul class="flex">
-        <li class="px-4 tab-active">
+        <li class="px-4 {{ Route::is('dashboard') ? 'tab-active' : '' }}">
             <a href="/" class="px-2 block my-4">Dashboard</a>
         </li class="px-4">
         @auth
-            <li class="px-4"><a href="{{ route('cart') }}" class="px-2 block my-4">Cart</a></li>
+            <li class="px-4 {{ Route::is('cart') ? 'tab-active' : '' }}">
+                <a href="{{ route('cart') }}" class="px-2 block my-4">Cart</a>
+            </li>
             <li class="px-4"><a href="" class="px-2 block my-4">Admin</a></li>
         @endauth
     </ul>

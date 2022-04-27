788936b48f1a7bba55d14feffdebd4cffc353cea
Michael Stanley <michstan96@gmail.com>
2022-04-27 15:34:40 +0700

    update game-detail page to show result from database

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,6 +1,8 @@
 @php
 $game->load('category', 'reviews');
+$game->reviews->load('user');
 
+$id = $game['id'];
 $image = $game['image'];
 $title = $game['title'];
 $desc = $game['description'];
@@ -10,6 +12,17 @@ $date = date('d M, Y', strtotime($game['created_at']));
 $category = $game->category->name;
 
 $reviews = $game->reviews->groupBy('recommend')->map->count();
+
+$relatedGames = $game->category
+    ->games()
+    ->limit(3)
+    ->whereNotIn('id', [$id])
+    ->get();
+
+$currReviews = $game
+    ->reviews()
+    ->limit(3)
+    ->get();
 
 @endphp
 
@@ -48,7 +61,9 @@ $reviews = $game->reviews->groupBy('recommend')->map->count();
             <p class="text-sm text-gray-500">All Reviews</p>
             @forelse ($reviews as $key => $review)
                 <h1 class="font-semibold text-lg">
-                    {{ $key == 1 ? $review . ' Recommended' : $review . ' Not Recommended' }}</h1>
+                    {{ $review }}
+                    {{ $key === 1 ? 'Recommended' : 'Not Recommended' }}
+                </h1>
             @empty
                 <h1 class="font-semibold text-lg">{{ '0 Review' }}</h1>
             @endforelse
@@ -60,18 +75,15 @@ $reviews = $game->reviews->groupBy('recommend')->map->count();
     </div>
 
     <div class="m-auto flex w-3/4 gap-3 mb-5">
-        <div class="">
-            <img src="{{ asset('/assets/games/Cyberpunk 2077/thumb.jpg') }}">
-            <p class="text-right">IDR 329.000</p>
-        </div>
-        <div class="">
-            <img src="{{ asset('/assets/games/God of War/thumb.jpg') }}">
-            <p class="text-right">IDR 559.000</p>
-        </div>
-        <div class="">
-            <img src="{{ asset('/assets/games/For Honor/thumb.jpg') }}">
-            <p class="text-right">IDR 418.000</p>
-        </div>
+        @foreach ($relatedGames as $game)
+            <div class="">
+                <img src="{{ asset('/assets/games/' . $game->image . '/thumb.jpg') }}">
+                <p class="text-right">
+                    {{ $game->price == 0 ? 'FREE' : 'IDR ' . $game->price * 1000 }}
+                </p>
+            </div>
+        @endforeach
+
     </div>
 
     <div class="m-auto w-3/4 bg-white p-5 rounded-md mb-5">
@@ -79,9 +91,9 @@ $reviews = $game->reviews->groupBy('recommend')->map->count();
         <form action="" method="post">
             @csrf
             <div class="mb-3">
-                <input type="checkbox" name="recommended" id="recommended">
+                <input type="radio" name="recommend" id="recommended">
                 <label for="recommended" class="mr-5">Recommended</label>
-                <input type="checkbox" name="not_recommended" id="not_recommended">
+                <input type="radio" name="recommend" id="not_recommended">
                 <label for="not_recommended">Not Recommended</label>
             </div>
             <textarea name="review" id="review" rows="10" class="border-black border-2 w-full mb-2"></textarea>
@@ -92,17 +104,14 @@ $reviews = $game->reviews->groupBy('recommend')->map->count();
 
     <div class="m-auto w-3/4 mb-3">
         <div class="flex justify-start w-full gap-3">
-            <div class="bg-white w-1/3 rounded-md p-3">
-                <h1 class="font-semibold">Abenbyy</h1>
-                <p class="text-gray-500 text-sm my-2">Recommended</p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, alias.</p>
-            </div>
-            <div class="bg-white w-1/3 rounded-md p-3">
-                <h1 class="font-semibold">Daniel</h1>
-                <p class="text-gray-500 text-sm my-2">Not Recommended</p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, quas ea a cumque accusantium aliquam.
-                </p>
-            </div>
+            @foreach ($currReviews as $review)
+                <div class="bg-white w-1/3 rounded-md p-3">
+                    <h1 class="font-semibold">{{ $review->user->name }}</h1>
+                    <p class="text-gray-500 text-sm my-2">{{ $review->recommend == 1 ? 'Recommended' : 'Not Recommended' }}
+                    </p>
+                    <p>{{ Str::limit($review->review, 100, $end = '...') }}</p>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection

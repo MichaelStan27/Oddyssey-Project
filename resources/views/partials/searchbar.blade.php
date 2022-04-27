facf35bd350302f411176452986dc3e94f8f6009
Daniel Wijaya <dwjy27@gmail.com>
2022-04-27 23:32:48 +0700

    Improve searchbar - Add hover effect - Remove ugly white outline whenever input get focus - Turn auto complete off

--- resources/views/partials/searchbar.blade.php
+++ resources/views/partials/searchbar.blade.php
@@ -1,6 +1,7 @@
 <form action="{{ route('search') }}" method="GET">
     <div class="w-full flex justify-center gap-3 mb-10">
-        <input type="text" name="q" class="rounded-md w-4/5 p-2 bg-gray-100 border-2 border-gray-300 outline-purple-300"
+        <input type="text" name="q" autocomplete="off"
+            class="rounded-md w-4/5 p-2 bg-gray-100 border-2 border-gray-300 hover:border-purple-300 outline-none focus:border-purple-300"
             placeholder="Search">
         <button type="submit"
             class="tracking-wider px-5 font-semibold bg-purple-700 rounded-lg text-white text-sm">SEARCH</button>

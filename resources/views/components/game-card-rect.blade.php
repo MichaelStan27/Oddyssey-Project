10c24b6054d58e2fb227248e82c97ff563728a9f
Daniel Wijaya <dwjy27@gmail.com>
2022-04-28 16:06:44 +0700

    Make card to be more general component

--- resources/views/components/game-card-rect.blade.php
+++ resources/views/components/game-card-rect.blade.php
@@ -4,18 +4,17 @@
 $image = $game->image;
 $title = $game->title;
 $cat = $game->category->name;
-$price = $game->price == 0 ? 'FREE' : "IDR {$game->price}";
 @endphp
 
-<a href="{{ route('game', $game) }}" class="block">
-    <div class="bg-white rounded-lg shadow-lg overflow-hidden flex items-center justify-between h-full">
-        <div class="flex max-w-fit">
-            <img src="{{ asset("/assets/games/{$image}/thumb.jpg") }}" class="h-24">
-            <div class="m-5 self-center">
-                <h1 class="font-medium text-xl">{{ $title }}</h1>
-                <p class="text-gray-500">{{ $cat }}</p>
-            </div>
+<div class="bg-white rounded-lg shadow-lg overflow-hidden flex items-center justify-between h-full">
+    <div class="flex max-w-fit">
+        <img src="{{ asset("/assets/games/{$image}/thumb.jpg") }}" class="h-24">
+        <div class="m-5 self-center">
+            <h1 class="font-medium text-xl">{{ $title }}</h1>
+            <p class="text-gray-500">{{ $cat }}</p>
         </div>
-        <p class="text-right mr-4 font-semibold text-lg text-gray-600">{{ $price }}</p>
     </div>
-</a>
+    <div class="mr-4 font-semibold text-gray-600">
+        {{ $slot }}
+    </div>
+</div>

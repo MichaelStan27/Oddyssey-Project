4e1e0f8fdc62538459152b4ae8614515c498e66f
Daniel Wijaya <dwjy27@gmail.com>
2022-04-28 17:03:39 +0700

    Update 'update-game' view to load previous value

--- resources/views/update-game.blade.php
+++ resources/views/update-game.blade.php
@@ -8,13 +8,14 @@
             <h1 class="text-center text-lg font-semibold mb-3">Update Game</h1>
             <form action="" method="post">
                 @csrf
-                <input type="text" name="title" id="title" placeholder="Title"
+                <input type="text" name="title" id="title" placeholder="Title" value="{{ $game->title }}"
                     class="rounded-md w-full p-2 bg-gray-100 border-2 mb-5 outline-purple-300 @error('title') border-red-500 @enderror">
                 @error('title')
                     <p class="text-red-500 text-sm">{{ $message }}</p>
                 @enderror
 
                 <input type="text" name="category" id="category" placeholder="Category"
+                    value="{{ $game->category->name }}"
                     class="rounded-md w-full p-2 bg-gray-100 border-2 mb-5 outline-purple-300 @error('category') border-red-500 @enderror">
                 @error('category')
                     <p class="text-red-500 text-sm">{{ $message }}</p>
@@ -23,7 +24,7 @@
                 <div class="flex mb-5">
                     <label for="price"
                         class="flex justify-center bg-slate-300 items-center rounded-sm text-sm text-gray-700 w-1/12">IDR</label>
-                    <input type="text" name="price" id="price" placeholder="Price"
+                    <input type="text" name="price" id="price" placeholder="Price" value="{{ $game->price }}"
                         class="rounded-sm w-full p-2 bg-gray-100 border-2 outline-purple-300 @error('price') border-red-500 @enderror">
                     @error('price')
                         <p class="text-red-500 text-sm">{{ $message }}</p>
@@ -50,15 +51,16 @@
 
                 <div class="mb-2">
                     <label for="desc" class="text-gray-700">Description</label>
-                    <textarea name="desc" id="desc" rows="8" class="border-black border-2 w-full"></textarea>
+                    <textarea name="desc" id="desc" rows="8" class="border-black border-2 w-full">{{ $game->description }}</textarea>
                     @error('desc')
                         <p class="text-red-500 text-sm mb-2">{{ $message }}</p>
                     @enderror
                 </div>
 
                 <button type="submit"
-                    class="rounded-md bg-slate-800 text-white text-sm px-5 py-2 font-semibold hover:bg-slate-900">UPDATE
-                    GAME</button>
+                    class="rounded-md bg-slate-800 text-white text-sm px-5 py-2 font-semibold hover:bg-slate-900">
+                    UPDATE GAME
+                </button>
             </form>
         </div>
     </div>

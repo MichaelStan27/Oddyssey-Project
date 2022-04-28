5fa6cc1243d55f5f75e1d75d3d5408882830d2d7
Michael Stanley <michstan96@gmail.com>
2022-04-28 19:32:32 +0700

    Improve search page -change title page -change foreach with forelse

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
+@section('title', 'Search')
 
 @section('content')
     <div class="container mx-auto flex flex-col mb-10">
         @include('partials.searchbar')
 
         <div class="grid grid-cols-5 gap-8 mb-10">
-            @foreach ($results as $game)
+            @forelse ($results as $game)
                 <x-game-card-square :game="$game"></x-game-card-square>
-            @endforeach
+            @empty
+                <h1 class="col-start-3 text-center text-2xl text-gray-500 font-semibold mt-2">
+                    Games not found.</h1>
+            @endforelse
         </div>
 
         {{ $results->links() }}

3aaf88159d9a69fd3ef4548cdf7f4c081d23d171
Daniel <60413613+arville27@users.noreply.github.com>
2022-05-09 18:21:50 +0700

    Follow exam requirement for fetching hot games - Most purchased games in the past week

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller {
@@ -16,9 +17,18 @@ class DashboardController extends Controller {
             ->limit(5)
             ->get();
 
+        $hot_games = Game::with('category')
+            ->selectRaw('games.id, games.title, games.price, games.description, games.image, games.category_id, count(purchased_at) as total_purchased')
+            ->join('transactions', 'transactions.game_id', '=', 'games.id')
+            ->where('purchased_at', '>=', Carbon::today()->subDays(7))
+            ->groupBy('games.id', 'games.title', 'games.price', 'games.description', 'games.image', 'games.category_id')
+            ->orderBy('total_purchased', 'DESC')
+            ->limit(8)
+            ->get();
+
         return view('dashboard', [
             'featuredGames' => $featured,
-            'hotGames' => Game::with('category')->limit(8)->get()
+            'hotGames' => $hot_games
         ]);
     }
 }

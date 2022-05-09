ec5b73931cba7f9084cebb60084660ca630a0200
Michael Stanley <michstan96@gmail.com>
2022-05-09 13:59:30 +0700

    define eloquent relationship for transaction model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Game;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model {
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function game() {
+        return $this->belongsTo(Game::class);
+    }
+}

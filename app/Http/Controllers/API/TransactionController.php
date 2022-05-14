dc4a9cd3b6230e8f8a615184eaaa49fb9403b904
Michael Stanley <michstan96@gmail.com>
2022-05-14 12:46:19 +0700

    fix response json on get transaction api

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
 class TransactionController extends Controller {
-    public function index() {
+    public function index(Request $request) {
         return Response::json([
-            'data' => Transaction::with('game')->get()
+            'data' => Transaction::with('game')->where('user_id', $request->user()->id)->get()
         ]);
     }
 }
